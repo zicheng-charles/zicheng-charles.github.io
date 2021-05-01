362e976b2e49ecd148dd007cae0aad295b38e9a9
DESKTOP-2CITHKF\92383 <zicheng.charles@foxmail.com>
2021-05-01 21:48:18 +0800

    完善note页面，增加一个安装opencv的笔记

---
+++ js/notelist.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $(".notelist").hover(function() {
+        $(this).css("background-color", "rgba(255,255,255, 0.3)");
+    }, function() {
+        $(this).css("background-color", "rgba(255,255,255, 0)");
+    });
+
+});
